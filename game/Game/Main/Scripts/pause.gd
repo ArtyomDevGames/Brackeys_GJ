31bd5b3ff9f3bbc5fefb882d2cb8cdebe7e6bf1c
Cblpok <glazed_cheese@inbox.ru>
2026-08-26 23:51:58 +0700

    Settings menu: animations, no functionality

--- game/Game/Main/Scripts/pause.gd
+++ game/Game/Main/Scripts/pause.gd
@@ -10,8 +10,9 @@ func _ready() -> void:
 func button_pressed() -> void:
 	var button = group.get_pressed_button()
 	
-	if button.button_name == "ContinueButton":
-		animation_player.play_backwards("ShowPause")
+	if button.button_name == "ContinueButton": animation_player.play_backwards("ShowPause")
+	elif button.button_name == "SettingsButton": show_settings()
+	elif button.button_name == "BackButton": hide_settings()
 
 func show_pause() -> bool:
 	visible = true
@@ -27,4 +28,18 @@ func hide_pause() -> bool:
 	await animation_player.animation_finished
 	NoTouchRect.visible = false
 	visible = false
+	return true
+
+func show_settings() -> bool:
+	NoTouchRect.visible = true
+	animation_player.play("ShowSettings")
+	await animation_player.animation_finished
+	NoTouchRect.visible = false
+	return true
+
+func hide_settings() -> bool:
+	NoTouchRect.visible = true
+	animation_player.play_backwards("ShowSettings")
+	await animation_player.animation_finished
+	NoTouchRect.visible = false
 	return true
