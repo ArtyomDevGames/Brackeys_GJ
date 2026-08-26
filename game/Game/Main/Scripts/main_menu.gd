3e22925b613a44720dcd261602d61af17fdd09fb
Cblpok <glazed_cheese@inbox.ru>
2026-08-26 22:55:00 +0700

    Pause menu animation

--- game/Game/Main/Scripts/main_menu.gd
+++ game/Game/Main/Scripts/main_menu.gd
@@ -13,6 +13,13 @@ func button_pressed() -> void:
 	var button = group.get_pressed_button()
 	
 	if button.button_name == "StartButton":
+		NoTouchRect.visible = true
 		Monitor.expand_monitor()
 		animation_player.play_backwards("ShowMenu")
 		await animation_player.animation_finished
+		animation_player.play("HideBackground")
+		await animation_player.animation_finished
+		$Background.visible = false
+		NoTouchRect.visible = false
+	elif button.button_name == "ExitButton":
+		get_tree().quit()
---
+++ game/Game/Main/Scripts/pause.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+@export var group : ButtonGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	pass 
+
+func button_pressed() -> void:
+	var button = group.get_pressed_button()
+	
+	if button.button_name == "ContinueButton":
+		animation_player.play_backwards("ShowPause")
+
+func show_pause() -> bool:
+	NoTouchRect.visible = true
+	animation_player.play("ShowPause")
+	await animation_player.animation_finished
+	NoTouchRect.visible = false
+	return true
